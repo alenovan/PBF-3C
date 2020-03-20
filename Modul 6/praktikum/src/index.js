27b3b9376a4d34e8e24f227a4b7fa1d6e14b0432
alptra <novannanega@gmail.com>
2020-03-20 08:34:06 +0700

    modul 6

---
+++ Modul 6/praktikum/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; 
+import { createStore, applyMiddleware, compose } from 'redux';
+import './index.css';
+import MainReducer from './Reducers/MainReducer' ;
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer) 
+ReactDOM.render(<Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById('root'));
+serviceWorker.unregister();
+ 
