9973e0f95a4b88e9274fe85e0bb404b569a48540
alptra <novannanega@gmail.com>
2020-05-08 00:09:30 +0700

    modul 11

---
+++ Modul 11/Tugas/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDj4iLohn3D1US79bk7Sc-PIb5b1c2g4hk",
+  authDomain: "tugaskampus1-16cd5.firebaseapp.com",
+  databaseURL: "https://tugaskampus1-16cd5.firebaseio.com",
+  projectId: "tugaskampus1-16cd5",
+  storageBucket: "tugaskampus1-16cd5.appspot.com",
+  messagingSenderId: "869186910796",
+  appId: "1:869186910796:web:011c5b534c1894498f92e9",
+  measurementId: "G-3FN27WPPEZ"
+};
+// 
+// firebase.analytics();
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
